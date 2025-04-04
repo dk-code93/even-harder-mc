d4887a6378c7897e3f910fdf5211ff1ad8c6ea5d
Daniel <dkcoder93@gmail.com>
2025-04-04 15:56:18 -0600

    removed some worldgen features

--- kubejs/startup_scripts/worldgen.js
+++ kubejs/startup_scripts/worldgen.js
@@ -4,7 +4,12 @@ WorldgenEvents.remove(event => {
     event.printFeatures('', 'minecraft:swamp')
     event.printFeatures('', 'minecraft:badlands')
     event.printFeatures('', 'minecraft:dark_forest')
-    event.printFeatures('', 'minecraft:mushroom_island')
+    event.printFeatures('', 'minecraft:mushroom_fields')
+    event.removeFeatureById('raw_generation', [
+        'immersive_weathering:fluvisol_patch_surface',
+        'immersive_weathering:fluvisol_patch_dry',
+        'immersive_weathering:humus_patch',
+    ])
     event.removeFeatureById('local_modifications', [
         'biomemakeover:mesmerite_boulder',
     ])
@@ -12,11 +17,25 @@ WorldgenEvents.remove(event => {
         'biomemakeover:mesmerite_underground',
         'biomemakeover:df_trees'
     ])
+    event.removeFeatureById('top_layer_modification', [
+        'biomemakeover:peat',
+        'biomemakeover:reeds',
+        'biomemakeover:itching_ivy',
+    ])
+    event.removeFeatureById('fluid_springs', [
+        'biomemakeover:blighted_balsa_placed'
+    ])
     event.removeFeatureById('vegetal_decoration', [
         'biomemakeover:swamp_cypress_trees_placed', 
         'biomemakeover:willow_trees_placed', 
         'biomemakeover:willow',
+        'biomemakeover:swamp_flowers',
+        'biomemakeover:small_and_flowered_pads',
         'biomemakeover:barrel_cactus',
         'biomemakeover:saguaro_cactus',
+        'biomemakeover:wild_mushrooms',
+        'biomemakeover:df_grass',
+        'biomemakeover:df_tall_grass',
+        'biomemakeover:df_flowers',
     ])
 });
