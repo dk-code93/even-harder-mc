39fba5107eeede2f3b41352c45b33af6035a0c6d
Daniel <dkcoder93@gmail.com>
2025-04-05 14:26:19 -0600

    removed some modded mob spawns

---
+++ kubejs/startup_scripts/spawns.js
@@ -0,0 +1,15 @@
+// priority: 1
+
+EntityEvents.checkSpawn("biomemakeover:blightbat", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:glowfish", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:mushroom_trader", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:scuttler", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:ghost", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:cowboy", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:decayed", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:tadpole", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:toad", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:lightning_bug", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:lightning_bug_alternate", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:rootling", e => e.success(false))
+EntityEvents.checkSpawn("biomemakeover:helmit_crab", e => e.success(false))
